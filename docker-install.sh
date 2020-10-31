aecfde0bbd9880f64524d5e20a10199d1d47ce81
Avik Kundu <1828008@kiit.ac.in>
2020-10-31 15:20:08 +0530

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-apt update -y
-apt install apt-transport-https ca-certificates curl software-properties-common
+apt update
+apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-apt update -y
+apt update
 apt-cache policy docker-ce
 apt install docker-ce -y
