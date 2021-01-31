bd9fed43b7ca1314057158abe2be985981ec1774
Avik Kundu <1828008@kiit.ac.in>
2021-01-31 15:57:48 +0530

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
-apt-get update
 
-apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common 
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-
-add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ \
-    ubuntu bionic \
-    stable"
-
-apt-get update
-
-apt-cache policy docker-ce
-
-apt-get install docker-ce -y
+apt update -y
+apt-get remove docker docker-engine docker.io -y
+apt install docker.io -y
+systemctl start docker
+systemctl enable docker
 
